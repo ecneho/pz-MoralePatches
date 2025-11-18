6cb1ac8fba76591aeb4f6bf2cd4eb2954c49a2bd
Ecneho <zzsim0ngg@gmail.com>
2025-11-18 23:53:28 +0200

    ZBundler migration. Added B42 crafting recipes

---
+++ 41/common/media/lua/server/items/Crafting_Support.lua
@@ -0,0 +1,14 @@
+local function generateRecipes(amount)
+    for i = 1, amount do
+        local functionName = "EmbroiderPatch" .. i
+        Recipe.OnCreate[functionName] = function(_, character)
+            ---@type InventoryItem
+            local patch = instanceItem("MoralePatches.MoralePatch")
+            patch:getVisual():setTextureChoice(i-1)
+            character:getInventory():DoAddItem(patch)
+            patch:synchWithVisual()
+        end
+    end
+end
+
+generateRecipes(65) -- total patches
